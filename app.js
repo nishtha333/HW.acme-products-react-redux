b37725bbd2d6b8003d5de70edf88669f0a155386
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-09-15 13:59:18 -0400

    React Setup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -5,3 +6,8 @@ module.exports = app;
 
 app.use(express.json());
 app.use('/api/products', require('./api/products'));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
